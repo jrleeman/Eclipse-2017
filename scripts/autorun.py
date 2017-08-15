c0ed3d83d7a480548bd84060150effbfde256162
J.R. Leeman <kd5wxb@gmail.com>
2017-08-15 15:08:55 -0600

    Update autorun

--- scripts/autorun.py
+++ scripts/autorun.py
@@ -28,17 +28,19 @@ while not triggered:
 
         print('Downloading data...')
         scripts = [['get_GOES.py', str(channel)] for channel in range(1, 17)]
-        scripts.insert(0, ['get_ASOS.py'])
+        scripts.insert(0, 'get_ASOS.py')
         with mp.Pool(processes=6) as pool:
             for script in scripts:
                 pool.apply_async(run_script, args=(script,), callback=log_result)
             pool.close()
             pool.join()
 
-        print('Making Animations...')
+        print('Making Animations/Images...')
         with mp.Pool(processes=4) as pool:
             scripts = [['goes_animations.py', str(channel)] for channel in range(1, 17)]
-            scripts.insert(0, ['temperature_change_map.py', 'temperature_map.py'])
+            scripts.insert(0, 'temperature_change_map.py')
+            scripts.insert(0, 'temperature_map.py')
+            scripts.insert(0, 'event_static_image.py')
             for script in scripts:
                 pool.apply_async(run_script, args=(script,), callback=log_result)
             pool.close()
